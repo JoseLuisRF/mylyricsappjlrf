cefbd908c635f6e8e0828c31d1aa1f3ef84606e6
Jose Ramos <jose.ramos@unosquare.com>
2017-11-04 21:20:12 -0600

    DataBindingAdapyter

---
+++ app/src/main/java/com/example/joseramos/lyricsappjlrf/presentation/adapters/CustomDataBindingAdapter.java
@@ -0,0 +1,25 @@
+package com.example.joseramos.lyricsappjlrf.presentation.adapters;
+
+
+import android.databinding.BindingAdapter;
+import android.support.v4.content.ContextCompat;
+import android.text.TextUtils;
+import android.widget.ImageView;
+
+import com.example.joseramos.lyricsappjlrf.R;
+import com.squareup.picasso.Picasso;
+
+public class CustomDataBindingAdapter {
+
+    @BindingAdapter("imageUrl")
+    public static void loadImage(ImageView imageView, String url) {
+        if (!TextUtils.isEmpty(url)) {
+            Picasso.with(imageView.getContext())
+                    .load(url)
+                    .placeholder(R.color.colorPrimary)
+                    .error(android.R.color.holo_red_dark)
+                    .fit()
+                    .into(imageView);
+        }
+    }
+}
