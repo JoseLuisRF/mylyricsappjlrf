2c1ed615c4f07b50434783354d4d9ae1d440b84b
Jose Ramos <jose.ramos@kroger.com>
2018-04-12 10:20:15 -0400

    [kotlin_mvvm] test: Create test for MusicCloudDataSourceImplTest  class

--- app/src/test/java/com/example/joseramos/lyricsappjlrf/data/api/datasource/MusicCloudDataSourceImplTest.kt
+++ app/src/test/java/com/example/joseramos/lyricsappjlrf/data/api/datasource/MusicCloudDataSourceImplTest.kt
@@ -1,20 +1,43 @@
 package com.example.joseramos.lyricsappjlrf.data.api.datasource
 
+import com.example.joseramos.lyricsappjlrf.data.api.MusicMatchApi
+import com.example.joseramos.lyricsappjlrf.data.api.model.BaseResponse
+import com.example.joseramos.lyricsappjlrf.data.api.model.GetTopSongsResponse
 import com.example.joseramos.lyricsappjlrf.data.repository.datasource.MusicCloudDataSource
+import org.junit.Before
 import org.junit.Test
+import org.mockito.ArgumentMatchers
+import org.mockito.BDDMockito.given
 import org.mockito.Mockito
-import org.mockito.Mockito.mock
-import org.mockito.Mockito.verify
+import org.mockito.Mockito.*
+import retrofit2.Call
 
 class MusicCloudDataSourceImplTest {
 
+    private val api = Mockito.mock(MusicMatchApi::class.java);
+    private lateinit var dataSource: MusicCloudDataSource
 
+    @Before
+    fun setup() {
+        dataSource = MusicCloudDataSourceImpl(api)
+    }
 
     @Test
     fun getTopSongs() {
-        val data  = mock(MusicCloudDataSource::class.java)
-        val result  = data.getTopSongs("test")
-        //verify(data).getTopSongs("test").isEmpty
+        val response = mock(Call::class.java) as Call<BaseResponse<GetTopSongsResponse>>
+
+        given(api.getTopSongs(ArgumentMatchers.anyInt(),
+                ArgumentMatchers.anyInt(),
+                ArgumentMatchers.anyString(),
+                ArgumentMatchers.anyInt())).willReturn(response)
+
+        dataSource.getTopSongs("test")
+
+        verify(api, times(1)).getTopSongs(
+                ArgumentMatchers.anyInt(),
+                ArgumentMatchers.anyInt(),
+                ArgumentMatchers.anyString(),
+                ArgumentMatchers.anyInt())
     }
 
     @Test
